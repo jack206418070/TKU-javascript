21d7fc1e25201efff1a146bd32977363e267bd9c
jack206418070 <jack90928@gmail.com>
2019-02-19 20:42:11 +0800

    w01-3-typeof

---
+++ w01-3-Data_Types/js/all.js
@@ -0,0 +1,27 @@
+const str = "jack"
+console.log(typeof str)
+
+const num = 123
+console.log(typeof num)
+
+const bool = true
+console.log(bool)
+
+const car = null
+console.log(typeof car)
+
+let river
+console.log(typeof river)
+
+const sym = Symbol()
+console.log(typeof sym)
+
+
+// ---------------- // 
+
+const arr = [1,2,3]
+console.log(typeof arr)
+
+const today = new Date()
+console.log(today)
+console.log(typeof today)
