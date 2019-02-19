27fdca019e68641281d7c97042f965a215189353
jack206418070 <jack90928@gmail.com>
2019-02-19 20:17:41 +0800

    w01-3

--- w01-2-Variable-let-const/js/all.js
+++ w01-2-Variable-let-const/js/all.js
@@ -7,14 +7,15 @@ name = '2019'
 var greeting
 console.log(greeting)
 
-
-var firstName = "Jack"
-var first_Name = "2019"
-var firstname = "02"
+// letter , numbets , _ , $
+// cannot use number
+var firstName = 'Jack'
+var first_Name = '2019'
+var firstname = '02'
 var firstnames
 
 
-let name2 = "Jack Cheng"
+let name2 = 'Jack Cheng'
 console.log(name2)
 name2 = '張家祥'
 console.log(name2)
@@ -26,3 +27,16 @@ name2 = '2019'
 // name3 = '張家祥'
 // console.log(name3)
 // name3 = '2019'
+
+const person = {
+    name: 'Jack',
+    age:26
+}
+console.log(person)
+person.name = 'Mary'
+console.log(person.name)
+
+const person2 = [2019,02,19,81,1]
+console.log(person2)
+person2[5] = 30
+console.log(person2)
