fc54d0f92db6404462f3b38308fb4b410a708b2f
jack206418070 <jack90928@gmail.com>
2019-02-19 19:51:36 +0800

    w01-2

--- w01-2-Variable-let-const/js/all.js
+++ w01-2-Variable-let-const/js/all.js
@@ -1,2 +1,28 @@
 var name = "Jack Cheng"
 console.log(name)
+name = '張家祥'
+console.log(name)
+name = '2019'
+
+var greeting
+console.log(greeting)
+
+
+var firstName = "Jack"
+var first_Name = "2019"
+var firstname = "02"
+var firstnames
+
+
+let name2 = "Jack Cheng"
+console.log(name2)
+name2 = '張家祥'
+console.log(name2)
+name2 = '2019'
+
+
+// const name3 = "Jack Cheng"
+// console.log(name3)
+// name3 = '張家祥'
+// console.log(name3)
+// name3 = '2019'
