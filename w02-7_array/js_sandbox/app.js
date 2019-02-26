c463b97d36331464a1c9ae8269d48fdccd71c622
Jack Cheng <jack90928@gmail.com>
2019-02-26 21:00:07 +0800

    w02

--- w02-7_array/js_sandbox/app.js
+++ w02-7_array/js_sandbox/app.js
@@ -1,7 +1,7 @@
 // Create some arrays
 const numbers = [43,56,33,23,44,36,5,2019,81,01];
 const numbers2 = new Array(22,45,33,76,54,30,32);
-const fruit = ['Apple', 'Banana', 'Orange', 'Pear','Pineapple'];
+const fruit = ['Apple', 'Banana', 'Orange', 'Pear','Pineapple','lemon'];
 const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1,name:'Jack'}, new Date()];
 
 let val;
@@ -23,21 +23,33 @@ val = numbers.indexOf(2019)
 console.log(val)
 // MUTATING ARRAYS
 // Add on to end
-
+val = numbers.push(87)
+console.log(val)
 // Add on to front
-
+val = numbers.unshift(87)
+console.log(val)
 // Take off from end
-
+val = numbers.pop()
+console.log(val)
 // Take off from front
-
+val = numbers.shift()
+console.log(val)
 // Splice values
-
+numbers.splice(0,1)
+console.log(numbers)
 // Reverse
-
+numbers.reverse()
+console.log(numbers)
 // Concatenate array
+val = numbers.concat(numbers2)
+console.log(val)
 
 // Sorting arrays
+val = fruit.sort()
+console.log(val)
 // Use the "compare function"
-
+val = numbers.sort(function(a,b){return a-b})
+console.log(val)
 // // Reverse sort
-
+val = numbers.sort(function(a,b){return b-a})
+console.log(val)
