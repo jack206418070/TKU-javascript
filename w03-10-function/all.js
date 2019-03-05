978d941a3cb1e26cd831c986fe4a938bc9c3a7bb
jack206418070 <44224865+jack206418070@users.noreply.github.com>
2019-03-05 19:34:07 +0800

    w03-2

--- w03-10-function/all.js
+++ w03-10-function/all.js
@@ -7,8 +7,19 @@ console.log(greet())
 console.log(greet('Jack','Cheng'))
 // FUNCTION EXPRESIONS
 
+const square = function(x){
+    return x*x
+}
 
+console.log(square(5))
 // IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
 
+;(function(){
+    console.log('IIFE Run')
+})()
 
+
+;(function greet(firstName = 'Alex', lastName = 'ComeOn'){
+    console.log( `Hello ${firstName + lastName}` )
+})('Jack','Cheng')
 // PROPERTY METHODS
