b767f69631340fc68db1b681488f8342ca522e29
jack206418070 <44224865+jack206418070@users.noreply.github.com>
2019-03-12 19:38:45 +0800

    w04-2

---
+++ w04-dom1_examinning_document_object/all.js
@@ -0,0 +1,31 @@
+let val
+
+val = document
+console.log(val)
+
+val = document.all
+console.log(val)
+
+val = document.all.length
+console.log(val)
+
+val = document.all[1]
+console.log(val)
+
+val = document.head
+console.log(val)
+
+val = document.body
+console.log(val)
+
+val = document.forms
+console.log(val)
+
+val = document.forms[0].id
+console.log(val)
+
+val = document.forms[0].method
+console.log(val)
+
+val = document.forms[0].action
+console.log(val)
